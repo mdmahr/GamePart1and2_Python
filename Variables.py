0354df11aeb98f16c70c9b93dc0b626be7b4b700
mdmahr <md.mahfuzur.rahaman@metropolia.fi>
2023-10-21 01:20:36 +0600

    Few Change

---
+++ Variables.py
@@ -0,0 +1,52 @@
+"""name = input("Please enter your name: ")
+print(f"Hello, {name}!")"""
+
+"""import math
+radius = float(input("Please enter the radius of the circle: "))
+area = math.pi * radius * radius
+print(f"The area of the circle with radius {radius} is {area:.2f}")"""
+
+
+
+"""length = float(input("Please enter the length of the rectangle: "))
+width = float(input("Please enter the width of the rectangle: "))
+perimeter = 2 * (length + width)
+area = length * width
+print(f"The perimeter of the rectangle is {perimeter:.2f}")
+print(f"The area of the rectangle is {area:.2f}")"""
+
+
+
+"""num1 = int(input("Please enter the first integer: "))
+num2 = int(input("Please enter the second integer: "))
+num3 = int(input("Please enter the third integer: "))
+total_sum = num1 + num2 + num3
+product = num1 * num2 * num3
+average = total_sum / 3
+print(f"The sum of the numbers is: {total_sum}")
+print(f"The product of the numbers is: {product}")
+print(f"The average of the numbers is: {average:.2f}")"""
+
+
+""""LOTS_IN_GRAMS = 13.3
+POUNDS_IN_LOTS = 32
+TALENTS_IN_POUNDS = 20
+
+talents = float(input("Enter talents: "))
+pounds = float(input("Enter pounds: "))
+lots = float(input("Enter lots: "))
+total_grams = (talents * TALENTS_IN_POUNDS * POUNDS_IN_LOTS * LOTS_IN_GRAMS +
+pounds * POUNDS_IN_LOTS * LOTS_IN_GRAMS +
+lots * LOTS_IN_GRAMS)
+kilograms = int(total_grams // 1000)
+grams = total_grams % 1000
+print(f"The weight in modern units: \n{kilograms} kilograms and {grams:.2f} grams.")"""
+
+import random
+x = str(random.randint(1, 6)) + str(random.randint(1, 6))
+print("" , x)
+
+
+
+
+
