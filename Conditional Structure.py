8922287c9d2482207e29e82330ab29a710c808d1
mdmahr <md.mahfuzur.rahaman@metropolia.fi>
2023-10-22 14:17:01 +0600

    Few Change

---
+++ Conditional Structure.py
@@ -0,0 +1,52 @@
+"""length = float(input("Enter the length of the zander in centimeters: "))
+if length < 42:
+    deficit = 42 - length
+    print(f"The zander does not meet the size limit. Please release it back into the lake.")
+    print(f"The fish is {deficit:.2f} centimeters below the size limit.")
+else:
+    print(f"The zander meets the size limit. Good catch!")"""
+
+
+"""cabin_class = input("Enter the cabin class (LUX, A, B, C): ")
+if cabin_class == "LUX":
+    print("upper-deck cabin with a balcony.")
+elif cabin_class == "A":
+    print("above the car deck, equipped with a window.")
+elif cabin_class == "B":
+    print("windowless cabin above the car deck.")
+elif cabin_class == "C":
+    print("windowless cabin below the car deck.")
+else:
+    print("Invalid cabin class")"""
+
+
+"""gender = input("Enter biological gender (male or female): ").lower()
+try:
+    hemoglobin = float(input("Enter hemoglobin value (g/l): "))
+
+    if gender == "female":
+        if hemoglobin < 117:
+                print("Hemoglobin value is low.")
+        elif hemoglobin > 155:
+                print("Hemoglobin value is high.")
+        else:
+                print("Hemoglobin value is normal.")
+    elif gender == "male":
+        if hemoglobin < 134:
+                print("Hemoglobin value is low.")
+        elif hemoglobin > 167:
+                print("Hemoglobin value is high.")
+        else:
+                print("Hemoglobin value is normal.")
+    else:
+            print("Invalid biological gender entered. Please enter male or female.")
+except ValueError:
+    print("Please enter a valid number for hemoglobin value.")"""
+
+
+"""year = int(input("Enter a year: "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"{year} is a leap year.")
+else:
+    print(f"{year} is not a leap year.")"""
+
